d4a51c1890b944aff42a2767f887639b882a7ca2
mode-durachyo <144421362+mode-durachyo@users.noreply.github.com>
2023-09-18 16:21:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-count = int(input())
-for k in range(count+1):
-    print(k)
-    k += 1
+def test(arr):
+    if (len(arr) == 0): return False;
+    if(arr[0] == arr[-1]): return True;
+    else: return False;
+print(test([1, 1]))
